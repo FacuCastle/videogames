6eecef8c613b76ef5b07af46e87e3d9ec826aec8
Facundo <facucastle4@gmail.com>
2023-12-31 04:34:46 -0300

    front

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,7 +8,7 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use("/videogames", videogamesRouter );
-router.use("/genres", genresRouter );
+router.use("/", videogamesRouter );
+router.use("/", genresRouter );
 
 module.exports = router;
